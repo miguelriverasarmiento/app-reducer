6284462423230accd8e4ae8204285a5d2c05f938
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-02-21 17:15:00 -0500

    Added components productItem and ShoppingCartProduct and reducers actionsTypes and ShoppingCart

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Learn useReducer</h1>
+      <h1 className='title'>Learn useReducer</h1>
+      <hr />
+      <h2 className='subtitle_products'>Products in stock</h2>
+      <div className='container_grid_products'></div>
+      <hr />
+      <h2 className='subtitle_shopping_cart'>Shopping cart</h2>
+      <div>
+        <button className='btn btn_totalPrice'>Total price</button>
+        <button className='btn btn_clearCart'>Clear cart</button>
+      </div>
+
+      <div className='container_grid_shopping_cart'></div>
     </>
   )
 }
